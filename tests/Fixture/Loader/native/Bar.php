6e970b42a5ce9f706723f81cc699da4690fcb886
CurtisBarogla <curtis_barogla@outlook.fr>
2018-04-02 09:10:31 +0200

    Rework from scratch again... :) (because i love it)

--- tests/Fixture/Loader/native/Bar.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Zoe\Component\Acl\Resource\Resource;
-use Zoe\Component\Acl\Resource\ResourceInterface;
-
-$resource = new Resource("Bar", ResourceInterface::BLACKLIST);
-$resource->addPermission("Bar");
-
-return $resource;
--- tests/Fixture/Loader/native/Foo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Zoe\Component\Acl\Resource\Resource;
-use Zoe\Component\Acl\Resource\ResourceInterface;
-
-$resource = new Resource("Foo", ResourceInterface::BLACKLIST);
-$resource->addPermission("Foo");
-
-return $resource;
