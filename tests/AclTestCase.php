fc2ca96e13c81802da6eff48ebd73312372f52b9
CurtisBarogla <curtis_barogla@outlook.fr>
2018-07-15 15:50:55 +0200

    Base class for all acl tests

---
+++ tests/AclTestCase.php
@@ -0,0 +1,26 @@
+<?php
+//StrictType
+declare(strict_types = 1);
+
+/*
+ * Ness
+ * Acl component
+ *
+ * Author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+
+namespace NessTest\Component\Acl;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Common to all tests for Acl component
+ *
+ * @author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+abstract class AclTestCase extends TestCase
+{
+    //
+}
