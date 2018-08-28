b7786159e5471a2548aa14fb7e844a79eceeabff
CurtisBarogla <curtis_barogla@outlook.fr>
2018-08-28 12:02:48 +0200

    Fallback processor entry loader implementation

---
+++ src/Resource/Loader/Entry/FallbackProcessorEntryLoaderWrapper.php
@@ -0,0 +1,69 @@
+<?php
+//StrictType
+declare(strict_types = 1);
+
+/*
+ * Ness
+ * Acl component
+ *
+ * Author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+
+namespace Ness\Component\Acl\Resource\Loader\Entry;
+
+use Ness\Component\Acl\Resource\EntryInterface;
+use Ness\Component\Acl\Resource\ResourceInterface;
+use Ness\Component\Acl\Exception\EntryNotFoundException;
+
+/**
+ * Fallback to a null processor if current processor for current entry does not return a valid entry
+ * 
+ * @author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+class FallbackProcessorEntryLoaderWrapper implements EntryLoaderInterface
+{
+    
+    /**
+     * Entry loader wrapped
+     * 
+     * @var EntryLoaderInterface
+     */
+    private $wrapped;
+    
+    /**
+     * Initialize loader
+     * 
+     * @param EntryLoaderInterface $wrapped
+     *   Entry loader wrapped
+     */
+    public function __construct(EntryLoaderInterface $wrapped)
+    {
+        $this->wrapped = $wrapped;
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Ness\Component\Acl\Resource\Loader\Entry\EntryLoaderInterface::load()
+     */
+    public function load(ResourceInterface $resource, string $entry, ?string $processor = null): EntryInterface
+    {
+        if(null === $processor)
+            return $this->wrapped->load($resource, $entry, $processor);
+        
+        try {
+            return $this->wrapped->load($resource, $entry, $processor);
+        } catch (EntryNotFoundException $e) {
+            try {
+                return $this->wrapped->load($resource, $entry, null);                                
+            } catch (EntryNotFoundException $e) {
+                $exception = new EntryNotFoundException("This entry '{$entry}' cannot be loaded for resource '{$resource->getName()}' through processor '{$processor}' nor global (null) processor");
+                $exception->setEntry($entry);
+                
+                throw $exception;
+            }
+        }
+    }
+
+}
---
+++ tests/Resource/Loader/Entry/FallbackProcessorEntryLoaderWrapperTest.php
@@ -0,0 +1,92 @@
+<?php
+//StrictType
+declare(strict_types = 1);
+
+/*
+ * Ness
+ * Acl component
+ *
+ * Author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+
+namespace NessTest\Component\Acl\Resource\Loader\Entry;
+
+use NessTest\Component\Acl\AclTestCase;
+use Ness\Component\Acl\Resource\Loader\Entry\EntryLoaderInterface;
+use Ness\Component\Acl\Resource\EntryInterface;
+use Ness\Component\Acl\Resource\ResourceInterface;
+use Ness\Component\Acl\Resource\Loader\Entry\FallbackProcessorEntryLoaderWrapper;
+use Ness\Component\Acl\Exception\EntryNotFoundException;
+
+/**
+ * FallbackProcessorEntryLoaderWrapper testcase
+ * 
+ * @see \Ness\Component\Acl\Resource\Loader\Entry\FallbackProcessorEntryLoaderWrapper
+ * 
+ * @author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+class FallbackProcessorEntryLoaderWrapperTest extends AclTestCase
+{
+    
+    /**
+     * @see \Ness\Component\Acl\Resource\Loader\Entry\FallbackProcessorEntryLoaderWrapper::load()
+     */
+    public function testLoad(): void
+    {
+        $entryFound = $this->getMockBuilder(EntryInterface::class)->getMock();
+        $resource = $this->getMockBuilder(ResourceInterface::class)->getMock();
+
+        $wrapped = $this->getMockBuilder(EntryLoaderInterface::class)->getMock();
+        $wrapped->expects($this->once())->method("load")->with($resource, "FooEntry", null)->will($this->returnValue($entryFound));
+        
+        $loader = new FallbackProcessorEntryLoaderWrapper($wrapped);
+        
+        $this->assertSame($entryFound, $loader->load($resource, "FooEntry", null));
+        
+        $wrapped = $this->getMockBuilder(EntryLoaderInterface::class)->getMock();
+        $wrapped->expects($this->once())->method("load")->with($resource, "FooEntry", "FooProcessor")->will($this->returnValue($entryFound));
+        
+        $loader = new FallbackProcessorEntryLoaderWrapper($wrapped);
+        
+        $this->assertSame($entryFound, $loader->load($resource, "FooEntry", "FooProcessor"));
+        
+        $wrapped = $this->getMockBuilder(EntryLoaderInterface::class)->getMock();
+        $wrapped
+            ->expects($this->exactly(2))
+            ->method("load")
+            ->withConsecutive([$resource, "FooEntry", "FooProcessor"], [$resource, "FooEntry", null])
+            ->will($this->onConsecutiveCalls($this->throwException(new EntryNotFoundException()), $entryFound));
+        
+        $loader = new FallbackProcessorEntryLoaderWrapper($wrapped);
+        
+        $this->assertSame($entryFound, $loader->load($resource, "FooEntry", "FooProcessor"));
+    }
+    
+                    /**_____EXCEPTIONS_____**/
+    
+    /**
+     * @see \Ness\Component\Acl\Resource\Loader\Entry\FallbackProcessorEntryLoaderWrapper::load()
+     */
+    public function testExceptionLoadWhenEntryCannotBeLoadedAtAll(): void
+    {
+        $this->expectException(EntryNotFoundException::class);
+        $this->expectExceptionMessage("This entry 'FooEntry' cannot be loaded for resource 'FooResource' through processor 'FooProcessor' nor global (null) processor");
+        
+        $resource = $this->getMockBuilder(ResourceInterface::class)->getMock();
+        $resource->expects($this->once())->method("getName")->will($this->returnValue("FooResource"));
+        
+        $wrapped = $this->getMockBuilder(EntryLoaderInterface::class)->getMock();
+        $wrapped
+            ->expects($this->exactly(2))
+            ->method("load")
+            ->withConsecutive([$resource, "FooEntry", "FooProcessor"], [$resource, "FooEntry", null])
+            ->will($this->throwException(new EntryNotFoundException()));
+        
+        $loader = new FallbackProcessorEntryLoaderWrapper($wrapped);
+        
+        $loader->load($resource, "FooEntry", "FooProcessor");
+    }
+    
+}
